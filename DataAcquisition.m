8a430aa7c1a91c8bb2d07dab93fafe4719b99c10
PCristian00 <cristianp9300@gmail.com>
2024-01-15 13:38:03 +0100

    Bozza esecuzione script da menu
    Prova (con errori) dell'esecuzione dei due script successivi dal menu del primo script

--- DataAcquisition.m
+++ DataAcquisition.m
@@ -2,19 +2,23 @@
 close all; clear m;
 l = true; % Mantiene il programma attivo finche' non viene scelto di uscire dal menu
 while (l)
+    close all;
     pause(1); % Pausa aggiunta per stile
     fprintf("SCEGLIERE OPERAZIONE\n")
-    fprintf("0 - Nuovo Utente\n") % Forse togliere?
-    fprintf("1 - Avvio\n") % Avvia la raccolta
-    fprintf("2 - Esci\n"); % Interrompe l'esecuzione del programma
+    fprintf("1 - Nuovo Utente\n") % Forse togliere?
+    fprintf("2 - Avvio\n") % Avvia la raccolta
+    fprintf("3 - Processa e Visualizza\n") % Esegue i restanti script
+    fprintf("0 - Esci\n"); % Interrompe l'esecuzione del programma
     fprintf("----------------\n");
     scelta = input(""); % Selezione del menu
     % disp(scelta);
 
     switch (scelta)
-        case 0 % Nuovo Utente
+        case 1 % Nuovo Utente
             fprintf("NON IMPLEMENTATO\n");
-        case 1 % Avvio
+        case 2 % Avvio
+            disp('Aprire MATLAB Mobile sul dispositivo e premere un tasto');
+            pause; % Attesa del tasto
             m = mobiledev;
             fprintf("Dispositivo %s connesso con successo.\n", m.Device)
             % Attivazione sensori
@@ -39,7 +43,15 @@ while (l)
             % Salvataggio dei dati in un file mat
             save('acc.mat', 'a');
             disp(['Dati salvati su ', 'acc.mat.']);
-        case 2 % Esci
+        case 3
+            % VERIFICARE CHIUSURA FIGURE CORRETTA
+            pause(1);
+            DataProcessing;
+            pause(1);
+            VisualizeSTD;
+
+        case 0 % Esci
+            % VERIFICARE PERCHé RIAPRE IL PROGRAMMA DOPO CASE 3
             fprintf("CHIUSURA PROGRAMMA\n");
             l = false; % Interrompe il ciclo
         otherwise, fprintf("Menu non trovato\n"); % L'utente torna al menu
