8aa0e89f2d12850cbee8869400bf65fb3ed88714
PCristian00 <cristianp9300@gmail.com>
2024-02-09 21:51:33 +0100

    Tutti indici in gest

--- DataProcessing.m
+++ DataProcessing.m
@@ -7,7 +7,7 @@ close all;
 % Nomi dei file da leggere
 filename = "samples.mat";
 metafilename = "metadata.csv";
-th = [0.6, 0.45, 0.3, 0.25]; % Valori di threshold per ogni sensore
+th = [0.45, 0.45, 0.3, 0.25]; % Valori di threshold per ogni sensore
 % (in ordine acc, mag, orientation, ang_vel)
 
 % Caricamento del file
@@ -231,6 +231,8 @@ filename = "movement" + name + "_.mat";
 % CALCOLO DEI DIFF : OK
 % PROVARE METODO CON CONFRONTO INCROCIATO
 % USARE 150 come distanza tra rossi e blu
+
+mov_diff=double.empty;
 a = 2;
 mov_diff(1) = movement_indices(1);
 
@@ -242,6 +244,7 @@ for i = 1:(size(movement_indices) - 1)
     end
 end
 
+still_diff=double.empty;
 a = 2;
 still_diff(1) = stillness_indices(1);
 
@@ -261,34 +264,39 @@ a = 1;
 % gest(1)=mov_diff(1,1);
 
 % Inizializza gest ad array vuoto di interi
-%gest=double.empty;
+gest=double.empty;
 % disp("QUO");
 for i = 1:(size(mov_diff, 2))
     % disp("QUI!");
     for j = 1:(size(still_diff, 2))
         % disp("QUE")
-        if ((still_diff(j)-mov_diff(i))>20)
-           % fprintf("fase 1: BLU > ROSSO\n")
-           fprintf(still_diff(j)+">"+mov_diff(i)+"\n")
+        if still_diff(j) > mov_diff(i)
+            fprintf("fase 1: BLU > ROSSO\n")
+            fprintf(still_diff(j)+">"+mov_diff(i)+"\n")
+            if(a==1), gest(a)=mov_diff(i);
+                a=a+1;
+            end
             for k = i:(size(mov_diff, 2))
-            %    disp("INIZIO FASE 2")
+                % disp("INIZIO FASE 2")
                 if (mov_diff(k) > still_diff(j))
-                %    fprintf("fase 2: ROSSO > BLU\n")
+                    %    fprintf("fase 2: ROSSO > BLU\n")
                     fprintf(mov_diff(k)+">"+still_diff(j)+"\n")
-                  %  disp("INIZIO FASE 3")
+                    %  disp("INIZIO FASE 3")
                     if (mov_diff(k) - 1 - still_diff(j) > 100)
                         fprintf("Indice "+mov_diff(k)+" Differenza : "+(mov_diff(k) - 1 - still_diff(j))+">100 (SALVATO)\n")
-                        gest(a)=still_diff(j)-1;
-                        a = a+1;
+                        gest(a) = still_diff(j) - 1;
+                        a = a + 1;
                         gest(a) = mov_diff(k);
                         a = a + 1;
-                        
                         break;
-                    else, fprintf("Indice "+mov_diff(k)+" Differenza : "+(mov_diff(k) - 1 - still_diff(j))+"<100\n")
+                    else
+                        if k == size(mov_diff, 2), gest(a) = still_diff(j+1) - 1;
+                        else, fprintf("Indice "+mov_diff(k)+" Differenza : "+(mov_diff(k) - 1 - still_diff(j))+"<100\n")
+                        end
                     end
                     break
-                % else, fprintf(mov_diff(k)+"<"+still_diff(j)+"\n")
-                end                
+                    % else, fprintf(mov_diff(k)+"<"+still_diff(j)+"\n")
+                end
             end
             break
         end
@@ -298,57 +306,57 @@ end
 % DA MIGLIORARE
 % gest(8)=still_diff(size(still_diff,2))-1;
 
-    % disp(gest)
+% disp(gest)
 
-    % SCARTO DEI FALSI POSITIVI: PRIMO METODO PROVATO
-    % NON FUNZIONANTE AL 100%
-    % Per ogni elemento dell'array delle differenze, si confronta il successivo
-    % e l'elemento scartato in precedenza per vedere se sia un falso positivo
-    % (cambio quiete-movimento in un lasso di tempo inferiore ai 150 punti??)
+% SCARTO DEI FALSI POSITIVI: PRIMO METODO PROVATO
+% NON FUNZIONANTE AL 100%
+% Per ogni elemento dell'array delle differenze, si confronta il successivo
+% e l'elemento scartato in precedenza per vedere se sia un falso positivo
+% (cambio quiete-movimento in un lasso di tempo inferiore ai 150 punti??)
 
-    % CAPIRE SE RIUTILIZZABILE ANCHE PER STILLNESS
-    % q = 0;
-    % for i = 1:((size(mov_diff, 2) - 1))
-    %     % fprintf("Diff (%d) = %d\n", i, mov_diff(i));
-    %     if (mov_diff(i+1) < (mov_diff(i) + 200))
-    %         % fprintf("Diff (%d+1) = %d\n", i, mov_diff(i+1));
-    %         % fprintf("Minore di diff %d\n", i);
-    %         q = mov_diff(i+1);
-    %         mov_diff(i+1) = 0;
-    %     else if (mov_diff(i+1) < q + 200)
-    %             q = mov_diff(i+1);
-    %             mov_diff(i+1) = 0;
-    %     end
-    %     end
-    % end
-    %
-    % % Stillness
-    % q=0;
-    % for i = 1:((size(still_diff, 2) - 1))
-    %     % fprintf("Diff (%d) = %d\n", i, mov_diff(i));
-    %     if (still_diff(i+1) < (still_diff(i) + 200))
-    %         % fprintf("Diff (%d+1) = %d\n", i, mov_diff(i+1));
-    %         % fprintf("Minore di diff %d\n", i);
-    %         q = still_diff(i+1);
-    %         still_diff(i+1) = 0;
-    %     else if (still_diff(i+1) < q + 200)
-    %             q = still_diff(i+1);
-    %             still_diff(i+1) = 0;
-    %     end
-    %     end
-    % end
+% CAPIRE SE RIUTILIZZABILE ANCHE PER STILLNESS
+% q = 0;
+% for i = 1:((size(mov_diff, 2) - 1))
+%     % fprintf("Diff (%d) = %d\n", i, mov_diff(i));
+%     if (mov_diff(i+1) < (mov_diff(i) + 200))
+%         % fprintf("Diff (%d+1) = %d\n", i, mov_diff(i+1));
+%         % fprintf("Minore di diff %d\n", i);
+%         q = mov_diff(i+1);
+%         mov_diff(i+1) = 0;
+%     else if (mov_diff(i+1) < q + 200)
+%             q = mov_diff(i+1);
+%             mov_diff(i+1) = 0;
+%     end
+%     end
+% end
+%
+% % Stillness
+% q=0;
+% for i = 1:((size(still_diff, 2) - 1))
+%     % fprintf("Diff (%d) = %d\n", i, mov_diff(i));
+%     if (still_diff(i+1) < (still_diff(i) + 200))
+%         % fprintf("Diff (%d+1) = %d\n", i, mov_diff(i+1));
+%         % fprintf("Minore di diff %d\n", i);
+%         q = still_diff(i+1);
+%         still_diff(i+1) = 0;
+%     else if (still_diff(i+1) < q + 200)
+%             q = still_diff(i+1);
+%             still_diff(i+1) = 0;
+%     end
+%     end
+% end
 
-    % Rimuove gli elementi uguali a 0 da diff
-    mov_diff = mov_diff(mov_diff ~= 0);
-    still_diff = still_diff(still_diff ~= 0);
+% Rimuove gli elementi uguali a 0 da diff
+mov_diff = mov_diff(mov_diff ~= 0);
+still_diff = still_diff(still_diff ~= 0);
 
-    % mov_diff = filterData(movement_indices, 150);
-    %
-    % % CAPIRE SE filterData RIUTILIZZABILE (CAMBIARE OFFSET?)
-    % still_diff = filterData(stillness_indices, 0);
+% mov_diff = filterData(movement_indices, 150);
+%
+% % CAPIRE SE filterData RIUTILIZZABILE (CAMBIARE OFFSET?)
+% still_diff = filterData(stillness_indices, 0);
 
-    % Salvataggio delle diff (AGGIORNARE CSV CON I VALORI OTTENUTI)
-    save(filename, "mov_diff", "still_diff", "movement_indices", "stillness_indices", "gest");
+% Salvataggio delle diff (AGGIORNARE CSV CON I VALORI OTTENUTI)
+save(filename, "mov_diff", "still_diff", "movement_indices", "stillness_indices", "gest");
 
 end
 
@@ -356,7 +364,7 @@ end
 % function diff = filterData(data, offset)
 % a = 2;
 % diff(1) = data(1);
-% 
+%
 % % Ricerca e filtraggio delle differenze maggiori di 1
 % for i = 1:(size(data) - 1)
 %     if (data(i+1) ~= data(i) + 1)
@@ -369,7 +377,7 @@ end
 % % Per ogni elemento dell'array delle differenze, si confronta il successivo
 % % e l'elemento scartato in precedenza per vedere se sia un falso positivo
 % % (cambio quiete-movimento in un lasso di tempo inferiore ai 150 punti??)
-% 
+%
 % % CAPIRE SE RIUTILIZZABILE ANCHE PER STILLNESS
 % for i = 1:((size(diff, 2) - 1))
 %     fprintf("Diff (%d) = %d\n", i, diff(i));
